915d903d8a595a2437018d820a0f67cc93fe244f
Shotokan ZH <shotokanzh@gmail.com>
2015-06-29 11:17:40 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+token=$(cat token.txt);
+timeout=60;
+log="hook.log"
+#don't edit below!!
+update_url="https://api.telegram.org/bot${token}/getUpdates?timeout=${timeout}&offset=";
+offset=0;
+
+while true;
+do
+	data=$(curl -s "${update_url}${offset}");
+	if [ $? -eq 0 ];
+	then
+		ok=$(echo "${data}" | jq -c -r -M ".ok");
+		if [ "$ok" = "true" ];
+		then
+			x=0;
+			result=$(echo "${data}" | jq -c -r -M ".result" | grep -vP "^\[\]$");
+			if [ $? -eq 0 ];
+			then
+				tmp=true;
+				while [ "$tmp" != "null" ];
+				do
+					tmp=$(echo "${data}" | jq -c -r -M ".result[${x}]");
+					x=$(( x + 1 ));
+					if [ "$tmp" != "null" ];
+					then
+						offset=$(echo "${tmp}" | jq -c -r -M ".update_id");
+						offset=$(( offset + 1 ));
+						echo "$tmp" >> "$log";
+					fi;
+				done;
+			fi;
+		fi;
+	fi;
+done;
